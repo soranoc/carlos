452d78a4751cf397abe8b9568624325e8c5c76b0
Clement SORANO <clement.sorano@etudiant.univ-lille1.fr>
2015-09-22 18:12:22 +0200

    Push REUSEADDR

--- webserver/socket.c
+++ webserver/socket.c
@@ -11,6 +11,7 @@
 int creer_serveur(int port){
   //Création de la socket
   int socket_serveur ;
+  int optval = 1;
   socket_serveur = socket ( AF_INET , SOCK_STREAM , 0);
   if ( socket_serveur == -1)
     {
@@ -22,6 +23,14 @@ int creer_serveur(int port){
   saddr.sin_family = AF_INET ; /* Socket ipv4 */
   saddr.sin_port = htons (port); /* Port d ' écoute */
   saddr.sin_addr.s_addr = INADDR_ANY ; /* écoute sur toutes les interfaces */
+
+  //Application de setsockopt sur la socket serveur
+  if (setsockopt(socket_serveur, SOL_SOCKET , SO_REUSEADDR , &optval , sizeof(int)) == -1)
+    {
+      perror("Can not set SO_REUSEADDR option");
+    }
+
+  //Bind de la socket
   if ( bind ( socket_serveur , ( struct sockaddr *)& saddr , sizeof ( saddr )) == -1)
     {
       perror ( " bind socket_serveur " );
